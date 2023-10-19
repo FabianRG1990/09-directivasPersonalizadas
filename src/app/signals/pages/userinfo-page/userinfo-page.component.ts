6edba6ffb1baf175f9e430a43ca82df65b544118
Fabian Rodriguez <fabian.soy1990@gmail.com>
2023-10-18 21:18:29 -0600

    manejo de errores en informacion de usuarios

--- src/app/signals/pages/userinfo-page/userinfo-page.component.ts
+++ src/app/signals/pages/userinfo-page/userinfo-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -14,6 +14,11 @@ export class UserinfoPageComponent implements OnInit {
   public currentUser = signal<User|undefined>(undefined);
   public userWasFound = signal(true);
 
+  public fullName = computed ( () => {
+    if( !this.currentUser() ) return 'Usuario no encontrado';
+  return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+  } )
+
   ngOnInit(): void {
     this.loadUser( this.userId() )
   }
@@ -25,9 +30,13 @@ export class UserinfoPageComponent implements OnInit {
     this.currentUser.set(undefined);
 
     this.userService.getUserById(id)
-    .subscribe( user => {
-      this.currentUser.set(user)
-    } )
+    .subscribe( {
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => this.userWasFound.set(false)
+    });
 
   }
 
